9799b07c282532990dfea5081789eec07839e36d
Jainam <jainamrupani04@gmail.com>
2024-02-20 22:23:02 +0530

    Added Deposit.java

---
+++ src/Bank/Management/System/Deposit.java
@@ -0,0 +1,12 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class Deposit extends JFrame {
+    Deposit(){
+        
+    }
+    public static void main(String[] args) {
+        new Deposit();
+    }
+}
