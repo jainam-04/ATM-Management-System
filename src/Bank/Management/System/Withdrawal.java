db47f5eb2b16f963f893be2f0081559416e18d02
Jainam <jainamrupani04@gmail.com>
2024-02-22 16:45:21 +0530

    Added Withdrawal.java

---
+++ src/Bank/Management/System/Withdrawal.java
@@ -0,0 +1,12 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class Withdrawal extends JFrame {
+    Withdrawal(){
+
+    }
+    public static void main(String[] args) {
+        new Withdrawal();
+    }
+}
