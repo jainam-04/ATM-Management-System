cae5647daac5333e73509bfc4c6a511c4685c785
Jainam <jainamrupani04@gmail.com>
2024-02-23 13:42:39 +0530

    Added ChangePin.java

---
+++ src/Bank/Management/System/ChangePin.java
@@ -0,0 +1,12 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class ChangePin extends JFrame {
+    ChangePin(){
+        
+    }
+    public static void main(String[] args) {
+        new ChangePin();
+    }
+}
