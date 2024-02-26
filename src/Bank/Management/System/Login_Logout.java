7d051dd7264ce9c58811267bfab785d8a2299778
Jainam <jainamrupani04@gmail.com>
2024-02-26 17:32:10 +0530

    Added Login_Logout.java

---
+++ src/Bank/Management/System/Login_Logout.java
@@ -0,0 +1,12 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class Login_Logout extends JFrame {
+    Login_Logout(){
+        
+    }
+    public static void main(String[] args) {
+        new Login_Logout();
+    }
+}
