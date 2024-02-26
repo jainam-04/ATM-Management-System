7e6d62a27de86d661b35f468ff188627cba26a09
Jainam <jainamrupani04@gmail.com>
2024-02-26 16:53:38 +0530

    Added WhoAreYou.java

---
+++ src/Bank/Management/System/WhoAreYou.java
@@ -0,0 +1,12 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class WhoAreYou extends JFrame {
+    WhoAreYou(){
+        
+    }
+    public static void main(String[] args) {
+        new WhoAreYou();
+    }
+}
