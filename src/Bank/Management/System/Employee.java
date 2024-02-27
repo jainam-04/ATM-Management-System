21fffede95820976ad6256bc65a3ef9e65adb356
Jainam <jainamrupani04@gmail.com>
2024-02-27 19:21:00 +0530

    Added Employee.java

---
+++ src/Bank/Management/System/Employee.java
@@ -0,0 +1,12 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class Employee extends JFrame {
+    Employee(){
+        
+    }
+    public static void main(String[] args) {
+        new Employee();
+    }
+}
