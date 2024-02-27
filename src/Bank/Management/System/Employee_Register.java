2f19ac4766997be09f09097bba06db45d469b47a
Jainam <jainamrupani04@gmail.com>
2024-02-27 19:59:22 +0530

    Added Employee_Register.java

---
+++ src/Bank/Management/System/Employee_Register.java
@@ -0,0 +1,12 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class Employee_Register extends JFrame {
+    Employee_Register(){
+        
+    }
+    public static void main(String[] args) {
+        new Employee_Register();
+    }
+}
