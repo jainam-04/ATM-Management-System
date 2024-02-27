05ad9c1449377b7bcd22e84914e16092c33b93d8
Jainam <jainamrupani04@gmail.com>
2024-02-27 19:30:34 +0530

    Added Employee_Login.java

---
+++ src/Bank/Management/System/Employee_Login.java
@@ -0,0 +1,12 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class Employee_Login extends JFrame {
+    Employee_Login(){
+        
+    }
+    public static void main(String[] args) {
+        new Employee_Login();
+    }
+}
