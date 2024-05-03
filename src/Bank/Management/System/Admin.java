dd0b2f67df1a3e1e0ca0b7cc6878d55377471240
Jainam <jainamrupani04@gmail.com>
2024-05-03 10:50:11 +0530

    Made some modifications

--- src/Bank/Management/System/Admin.java
+++ src/Bank/Management/System/Admin.java
@@ -6,7 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Admin extends JFrame implements ActionListener {
-    JButton buttonLogin, buttonLogout, buttonRegister;
+    JLabel label1, label2, label3;
+    JTextField textField2;
+    JPasswordField passwordField3;
+    JButton button1, button2;
     Admin(){
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icon/bank.png"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
@@ -22,6 +25,50 @@ public class Admin extends JFrame implements ActionListener {
         iimage.setBounds(630, 350, 100, 100);
         add(iimage);
 
+        label1 = new JLabel("Admin Panel");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("AvantGarde", Font.BOLD, 38));
+        label1.setBounds(270, 125, 450, 40);
+        add(label1);
+
+        label2 = new JLabel("Admin Id: ");
+        label2.setFont(new Font("Railway", Font.BOLD, 28));
+        label2.setForeground(Color.WHITE);
+        label2.setBounds(150, 190, 375, 30);
+        add(label2);
+
+        textField2 = new JTextField();
+        textField2.setBounds(325, 190, 230, 30);
+        textField2.setFont(new Font("Arial", Font.BOLD, 14));
+        add(textField2);
+
+        label3 = new JLabel("Password: ");
+        label3.setFont(new Font("Railway", Font.BOLD, 28));
+        label3.setForeground(Color.WHITE);
+        label3.setBounds(150, 250, 375, 30);
+        add(label3);
+
+        passwordField3 = new JPasswordField();
+        passwordField3.setBounds(325, 250, 230, 30);
+        passwordField3.setFont(new Font("Arial", Font.BOLD, 14));
+        add(passwordField3);
+
+        button1 = new JButton("SIGN IN");
+        button1.setFont(new Font("Arial", Font.BOLD, 14));
+        button1.setBounds(320, 300, 100, 30);
+        button1.setForeground(Color.WHITE);
+        button1.setBackground(Color.BLACK);
+        button1.addActionListener(this);
+        add(button1);
+
+        button2 = new JButton("CLEAR");
+        button2.setFont(new Font("Arial", Font.BOLD, 14));
+        button2.setBounds(450, 300, 100, 30);
+        button2.setForeground(Color.WHITE);
+        button2.setBackground(Color.BLACK);
+        button2.addActionListener(this);
+        add(button2);
+
         ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("Icon/backbg.png"));
         Image iii2 = iii1.getImage().getScaledInstance(850, 480, Image.SCALE_DEFAULT);
         ImageIcon iii3 = new ImageIcon(iii2);
@@ -29,62 +76,39 @@ public class Admin extends JFrame implements ActionListener {
         iiimage.setBounds(0, 0, 850, 480);
         add(iiimage);
 
-        JLabel label1 = new JLabel("WELCOME TO ATM");
-        label1.setForeground(Color.WHITE);
-        label1.setFont(new Font("AvantGarde", Font.BOLD, 38));
-        label1.setBounds(230, 125, 450, 40);
-        iiimage.add(label1);
-
-        JLabel label2 = new JLabel("(FOR ADMIN)");
-        label2.setForeground(Color.BLACK);
-        label2.setFont(new Font("AvantGarde", Font.BOLD, 18));
-        label2.setBounds(350, 200, 450, 30);
-        iiimage.add(label2);
-
-        buttonLogin = new JButton("LOGIN");
-        buttonLogin.setBackground(Color.BLACK);
-        buttonLogin.setForeground(Color.WHITE);
-        buttonLogin.setBounds(200, 300, 150, 30);
-        buttonLogin.setFont(new Font("Arial", Font.BOLD, 14));
-        buttonLogin.addActionListener(this);
-        iiimage.add(buttonLogin);
-
-        buttonLogout = new JButton("LOGOUT");
-        buttonLogout.setFont(new Font("Arial", Font.BOLD, 14));
-        buttonLogout.setBounds(400, 300, 170, 30);
-        buttonLogout.setBackground(Color.BLACK);
-        buttonLogout.setForeground(Color.WHITE);
-        buttonLogout.addActionListener(this);
-        iiimage.add(buttonLogout);
-
-        buttonRegister = new JButton("REGISTER");
-        buttonRegister.setForeground(Color.WHITE);
-        buttonRegister.setFont(new Font("Arial", Font.BOLD, 14));
-        buttonRegister.setBackground(Color.BLACK);
-        buttonRegister.setBounds(280, 350, 200, 30);
-        buttonRegister.addActionListener(this);
-        iiimage.add(buttonRegister);
-
         setLayout(null);
         setSize(850, 480);
-        setLocation(450, 200);
         setUndecorated(true);
+        setLocation(450, 200);
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == buttonLogin){
-            new Admin_Login();
-            setVisible(false);
+        try{
+            if(e.getSource() == button1){
+                String adminId = textField2.getText();
+                String password = String.valueOf(passwordField3.getPassword());
+                if(adminId.equals("") || password.equals("")){
+                    JOptionPane.showMessageDialog(null, "Fill all the fields!");
+                }
+                else{
+                    if(adminId.equals("Jainam04") && password.equals("Rupani@0410")){
+                        new Login();
+                        setVisible(false);
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null, "Incorrect Id and Password!");
+                    }
+                }
+            }
+            else if(e.getSource() == button2){
+                textField2.setText("");
+                passwordField3.setText("");
+            }
         }
-        else if(e.getSource() == buttonLogout){
-            JOptionPane.showMessageDialog(null, "You are logged out!");
-            setVisible(false);
-        }
-        else if(e.getSource() == buttonRegister){
-            new AdminRegister();
-            setVisible(false);
+        catch(Exception E){
+            E.printStackTrace();
         }
     }
 
